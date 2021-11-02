0ebf4f1cfd00a415d764cf4519f05dc07ddb187b
phartex <fateruvictor96@gmail.com>
2021-11-02 17:01:06 +0100

    'tobi'

--- src/component/View.js
+++ src/component/View.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import {useParams, Link} from "react-router-dom";
+import axios from "axios";
+import "./styles/View.css"
 
 const View = () => {
+    const [user, setUser] = useState(null);
+    const {id} = useParams()
+
+    useEffect(() =>{
+        if (id) {
+            getSingleUser(id);
+        }
+    },[id]);
+
+    const getSingleUser = async (id) => {
+        const response = await axios.get('http://localhost:3000/details')
+            if(response.status === 200){
+                setUser(response.data)
+                console.log(response.data)
+            }
+      }
+
+
     return (
         <div>
             view
